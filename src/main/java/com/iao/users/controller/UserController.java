bbbde54fb2db4e52b924f4836de3fe6500ad40b3
KNJ20 <karimanessane@gmail.com>
2024-01-11 23:37:23 +0100

    add user intefaces

--- src/main/java/com/iao/users/controller/UserController.java
+++ src/main/java/com/iao/users/controller/UserController.java
@@ -1,13 +1,29 @@
 package com.iao.users.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class UserController {
     @GetMapping("/manager")
     public void manager(){
         System.out.println("test");
 
+    }
+    @RequestMapping("/")
+    public String showHomePage() {
+        return "home";
+    }
+
+    @RequestMapping("/employee")
+    public String showEmployeePage() {
+        return "employee";
+    }
+
+    @RequestMapping("/edit")
+    public String editEmployeePage() {
+        return "editEmployee";
     }
 }
